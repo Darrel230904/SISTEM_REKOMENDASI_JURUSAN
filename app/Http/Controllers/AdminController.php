1b3a540d1a843e7a687315266bcdb0f8e9e70bf7
DarrelRadhitya <231210009@student.mercuabuana-yogya.ac.id>
2025-11-22 22:57:29 +0700

    memasang template admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,39 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function dashboard() {
+        return view('admin.dashboard');
+    }
+    
+    public function charts() {
+        return view('admin.charts');
+    }
+    
+    public function tables() {
+        return view('admin.tables');
+    }
+    
+    public function login() {
+        return view('admin.auth.login');
+    }
+    
+    public function register() {
+        return view('admin.auth.register');
+    }
+    
+    public function forgotPassword() {
+        return view('admin.auth.forgot-password');
+    }
+    
+    public function error401() {
+        return view('admin.errors.401');
+    }
+    
+    public function error404() {
+        return view('admin.errors.404');
+    }
+    
+    public function error500() {
+        return view('admin.errors.500');
+    }
 }
